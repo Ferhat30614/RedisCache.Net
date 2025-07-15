39ce5972fa5d64680668e9ebcc9b6b2ad324fc49
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-07-15 21:31:03 +0400

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            _memoryCache.Remove("zaman");
+            //if (!String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            //{
+
+            //    _memoryCache.Remove("zaman");
+
+            //}
+
+            if (!_memoryCache.TryGetValue("zaman", out string zamanCache))
+            {
+
+
+                Console.WriteLine("zaman cache  =  null");
+
+            }
+
+            Console.WriteLine("zaman cache  = "+zamanCache);
+
+            
+
+            return View();
+        }
+        public IActionResult Show()
+        {
+
+            var sonuc= _memoryCache.GetOrCreate<string>("zaman", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+
+            Console.WriteLine("sonucum "+ sonuc);
+
+            ViewBag.Zaman=_memoryCache.Get<string>("zaman");
+
+            return View();
+        }
+    }
+}
