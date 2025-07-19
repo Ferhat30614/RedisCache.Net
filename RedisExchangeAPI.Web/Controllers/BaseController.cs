b5970a61868913355efdcf7efcee5b7893bf86e0
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-07-19 19:30:53 +0400

    Kod tekrarındansa bir base contoroller oluşturup onu miras aldım ...

---
+++ RedisExchangeAPI.Web/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly RedisService _redisService;
+        protected IDatabase db;
+      
+
+        public BaseController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDatabase(3);
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RedisExchangeAPI.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+        private string listKey = "SortedSetName";
+        public HashTypeController(RedisService redisService):base(redisService){
+        
+        
+        
+        }
+        
+  
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
