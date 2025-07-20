88f65efafb24e7e6c9ddb9b338ea9ec9f0fd7bde
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-07-21 02:33:27 +0400

    Api tarfıda tamam

---
+++ RedisExampleApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using RedisExampleApp.API.Models;
+using RedisExampleApp.API.Repositories;
+
+namespace RedisExampleApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _productRepository.GetAsync());     
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _productRepository.GetByIdAsync(id));
+        }   
+        
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            return Created(string.Empty,await _productRepository.CreateAsync(product));
+
+            // created() 201 Created HTTP status code’u döner. (yani “başarıyla oluşturuldu” demek)
+
+            //Opsiyonel olarak bir "Location" başlığı ekler → locationUrl parametresi. created(locationUrl,responseObject)
+
+            //Cevap body'sine de responseObject’i koyar.
+        }
+
+
+
+    }
+}
