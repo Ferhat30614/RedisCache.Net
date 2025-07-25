bb035fe3f15b0a2167fb491bcde73db07ee797c7
Ferhat30614 <149321307+Ferhat30614@users.noreply.github.com>
2025-07-25 13:10:23 +0400

    Caching ve databaseden getirme işlemleri

---
+++ RedisExampleApp.API/Repositories/ProductRepositoryWithCacheDecorator.cs
@@ -0,0 +1,68 @@
+﻿using RedisExampleApp.API.Models;
+using RedisExampleApp.Cache;
+using StackExchange.Redis;
+using System.Reflection;
+using System.Text.Json;
+
+namespace RedisExampleApp.API.Repositories
+{
+    public class ProductRepositoryWithCacheDecorator : IProductRepository
+    {
+
+        private const string hashKey = "productCaches";
+        private readonly IProductRepository _productRepository;        
+        private readonly RedisService _redisService;
+        private readonly IDatabase db;   
+
+        public ProductRepositoryWithCacheDecorator(IProductRepository repository, RedisService redisService,IDatabase database)
+        {
+            _productRepository = repository;
+            _redisService = redisService;
+            db=database;
+            db = _redisService.GetDataBase(2);
+        }
+
+        public Task<Product> CreateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async  Task<List<Product>> GetAsync()
+        {
+            if (!await db.KeyExistsAsync(hashKey))
+            {
+                return await LoadToCacheFromDbAsync();
+            }
+
+            var products = new List<Product>();
+
+            var a = db.HashGetAllAsync(hashKey);
+            
+
+
+
+        }
+
+        public Task<Product?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private async Task<List<Product>> LoadToCacheFromDbAsync()
+        {
+            var products= await _productRepository.GetAsync();
+
+            products.ForEach(p =>
+            {
+                 db.HashSetAsync(hashKey,p.Id,JsonSerializer.Serialize(p));
+            });
+
+            return products;    
+            
+        }
+
+
+
+    }
+}
